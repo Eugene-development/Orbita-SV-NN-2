44e77b7ec1589f853cd7eb1b740e6ab289a53761
indexpro24@gmail.com <indexpro24@gmail.com>
2022-04-13 11:05:43 +0300

    git init

--- app/src/lib/header/index.svelte
+++ app/src/lib/header/index.svelte
@@ -86,13 +86,24 @@
     <nav class="bg-slate-800">
         <div class="max-w-7xl mx-auto px-2 sm:px-6">
             <div class="relative flex items-center justify-between h-12">
-                <div class="flex items-center px-2 lg:px-0">
+                <div class="flex items-center px-1 lg:px-0">
                     <div class="hidden lg:block ">
-                        <div class="flex space-x-4">
+                        <div class="flex space-x-2">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <p class="text-gray-300 px-3 rounded-md text-base font-medium">8 (800) 555-8-111</p>
-                            <p class="text-gray-300 px-3 rounded-md text-base font-medium">8 (930) 688-88-62</p>
-                            <p class="text-gray-300 px-3 rounded-md text-base font-medium">8 (902) 303-36-54</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
+                            </svg>
+                            <p class="text-gray-300 pr-3 rounded-md text-lg font-medium">8 (800) 555-8-111</p>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
+                            </svg>
+                            <p class="text-gray-300 pr-3 rounded-md text-lg font-medium">8 (930) 688-88-62</p>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
+                            </svg>
+                            <p class="text-gray-300 pr-3 rounded-md text-lg font-medium">8 (902) 303-36-54</p>
                         </div>
                     </div>
                 </div>
@@ -106,7 +117,7 @@
                                     <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
                                 </svg>
                             </div>
-                            <input id="search" name="search" class="block w-full pl-10  py-1 border border-transparent rounded-md leading-5 bg-slate-100 text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:border-white focus:ring-white focus:text-gray-900 sm:text-sm" placeholder="Поиск товара" type="search">
+                            <input id="search" name="search" class="block w-full pl-10  py-1 border border-transparent rounded-md leading-5 bg-slate-200 text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-white focus:border-white focus:ring-white focus:text-gray-900 sm:text-sm" placeholder="Поиск товара" type="search">
                         </div>
                     </div>
                 </div>
@@ -136,44 +147,44 @@
                         </svg>
                     </button>
                 </div>
-<!--                <div class="hidden lg:block lg:ml-4">-->
-<!--                    <div class="flex items-center">-->
-<!--                        <button type="button" class="flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">-->
-<!--                            <span class="sr-only">View notifications</span>-->
-<!--                            &lt;!&ndash; Heroicon name: outline/bell &ndash;&gt;-->
-<!--                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">-->
-<!--                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />-->
-<!--                            </svg>-->
-<!--                        </button>-->
+                <div class="hidden lg:block lg:ml-4">
+                    <div class="flex items-center">
+                        <button type="button" class="flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                            <span class="sr-only">View notifications</span>
+                            <!-- Heroicon name: outline/bell -->
+                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                            </svg>
+                        </button>
 
-<!--                        &lt;!&ndash; Profile dropdown &ndash;&gt;-->
-<!--                        <div class="ml-4 relative flex-shrink-0">-->
-<!--                            <div>-->
-<!--                                <button type="button" class="bg-gray-800 rounded-full flex text-sm text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">-->
-<!--                                    <span class="sr-only">Open user menu</span>-->
-<!--                                    <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">-->
-<!--                                </button>-->
+                        <!-- Profile dropdown -->
+                        <div class="ml-4 relative flex-shrink-0">
+                            <div>
+                                <button type="button" class="bg-gray-800 rounded-full flex text-sm text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                    <span class="sr-only">Open user menu</span>
+                                    <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                </button>
+                            </div>
+
+                            <!--
+                              Dropdown menu, show/hide based on menu state.
+
+                              Entering: "transition ease-out duration-100"
+                                From: "transform opacity-0 scale-95"
+                                To: "transform opacity-100 scale-100"
+                              Leaving: "transition ease-in duration-75"
+                                From: "transform opacity-100 scale-100"
+                                To: "transform opacity-0 scale-95"
+                            -->
+<!--                            <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">-->
+<!--                                &lt;!&ndash; Active: "bg-gray-100", Not Active: "" &ndash;&gt;-->
+<!--                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>-->
+<!--                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>-->
+<!--                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>-->
 <!--                            </div>-->
-
-<!--                            &lt;!&ndash;-->
-<!--                              Dropdown menu, show/hide based on menu state.-->
-
-<!--                              Entering: "transition ease-out duration-100"-->
-<!--                                From: "transform opacity-0 scale-95"-->
-<!--                                To: "transform opacity-100 scale-100"-->
-<!--                              Leaving: "transition ease-in duration-75"-->
-<!--                                From: "transform opacity-100 scale-100"-->
-<!--                                To: "transform opacity-0 scale-95"-->
-<!--                            &ndash;&gt;-->
-<!--&lt;!&ndash;                            <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">&ndash;&gt;-->
-<!--&lt;!&ndash;                                &lt;!&ndash; Active: "bg-gray-100", Not Active: "" &ndash;&gt;&ndash;&gt;-->
-<!--&lt;!&ndash;                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>&ndash;&gt;-->
-<!--&lt;!&ndash;                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>&ndash;&gt;-->
-<!--&lt;!&ndash;                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>&ndash;&gt;-->
-<!--&lt;!&ndash;                            </div>&ndash;&gt;-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -359,7 +370,7 @@
                         <a href="https://vk.com/orbita_stroy" target="_blank" class="text-base text-slate-900 hover:text-red-800"> VK </a>
                     </nav>
                     <div class="flex items-center md:ml-12">
-                        <a class="ml-8 inline-flex items-center justify-center px-4 py-1 border border-transparent rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-800 shadow-lg shadow-indigo-700/50" href="/shop/cart"
+                        <a class="ml-8 inline-flex items-center justify-center px-4 py-1 border border-transparent rounded-md text-base font-medium text-white bg-slate-700 hover:bg-slate-800 shadow-lg shadow-slate-700/50" href="/shop/cart"
                            sveltekit:noscroll>
                             <svg class="mr-1 h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                  xmlns="http://www.w3.org/2000/svg">
@@ -367,7 +378,7 @@
                                       stroke-width="2" />
                             </svg>
                             Корзина
-                            <div class="ml-1 animate-pulse">({countLengthCart})</div>
+                           (<div class="mx-1 animate-pulse text-red-400">{countLengthCart}</div>)
                         </a>
                     </div>
 
