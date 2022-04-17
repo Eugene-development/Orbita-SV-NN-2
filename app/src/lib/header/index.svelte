26983d835128e8dc0ecc64a6fbe93085517a881e
indexpro24@gmail.com <indexpro24@gmail.com>
2022-04-17 10:38:07 +0300

    git init

--- app/src/lib/header/index.svelte
+++ app/src/lib/header/index.svelte
@@ -290,21 +290,21 @@
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 sm:h-5 md:h-6 w-4 sm:w-5 md:w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
                         </svg>
-                        <a href="+7-800-555-8-111" type="" class="text-gray-300 pl-1 rounded-md text-xs sm:text-base md:text-lg font-medium">8 800-555-8-111</a>
+                        <a href="tel:+78005558111" type="" class="text-gray-300 pl-1 rounded-md text-xs sm:text-base md:text-lg font-medium">8 800-555-8-111</a>
                     </div>
 
                     <div class="flex">
                         <svg xmlns="http://www.w3.org/2000/svg" class="hidden sm:block h-4 sm:h-5 md:h-6 w-4 sm:w-5 md:w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
                         </svg>
-                        <p class="text-gray-300 pl-1 rounded-md text-xs sm:text-base md:text-lg font-medium">8 930-688-88-62</p>
+                        <a href="tel:+79306888862" class="text-gray-300 pl-1 rounded-md text-xs sm:text-base md:text-lg font-medium">8 930-688-88-62</a>
                     </div>
 
                     <div class="flex">
                         <svg xmlns="http://www.w3.org/2000/svg" class="hidden sm:block h-4 sm:h-5 md:h-6 w-4 sm:w-5 md:w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
                         </svg>
-                        <p class="text-gray-300 pl-1 rounded-md text-xs sm:text-base md:text-lg font-medium">8 902-303-36-54</p>
+                        <a href="tel:+79023033654" class="text-gray-300 pl-1 rounded-md text-xs sm:text-base md:text-lg font-medium">8 902-303-36-54</a>
                     </div>
                 </div>
 
