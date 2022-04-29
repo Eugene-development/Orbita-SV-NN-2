ca8df6034a4d94e36a14cfa509d7fc6a2ed7ebe1
indexpro24@gmail.com <indexpro24@gmail.com>
2022-04-29 20:53:37 +0300

    git init

--- app/src/routes/category/[slug].svelte
+++ app/src/routes/category/[slug].svelte
@@ -17,6 +17,8 @@
         const data = resJSON.categories.data[0]
         const nameRubric = data.name
         const category = data.category
+
+        console.log(category)
         const text = data.text
         const title = 'Стройматериалы' + ' || ' + nameRubric + ' в Нижнем Новгороде'
         const description = nameRubric + ' в Нижнем Новгороде и области. ' + 'Приобретайте ' + nameRubric + ' по низким ценам.'
@@ -61,58 +63,177 @@
 <!--        </div>-->
 <!--    </div>-->
 
-    <div class="mx-4 flex  ">
-        <div class="flex w-1/3 max-w-sm flex-col flex-grow border-r border-slate-200 pt-8 pb-4 overflow-y-auto hidden xl:block">
-            <div class="mb-6 flex items-center flex-shrink-0 px-4">
-                <!--          <svg class="text-slate-500 mr-3 h-8 w-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"-->
-                <!--               xmlns="http://www.w3.org/2000/svg">-->
-                <!--            <path-->
-                <!--              d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"-->
-                <!--              stroke-linecap="round" stroke-linejoin="round"-->
-                <!--              stroke-width="2"/>-->
-                <!--          </svg>-->
-                <div class="text-slate-800 text-2xl">
-                    <strong>{ nameRubric }:</strong>
-                </div>
-            </div>
+    <div class="  ">
+<!--        <div class="flex w-1/3 max-w-sm flex-col flex-grow border-r border-slate-200 pt-8 pb-4 overflow-y-auto hidden xl:block">-->
+<!--            <div class="mb-6 flex items-center flex-shrink-0 px-4">-->
+<!--                &lt;!&ndash;          <svg class="text-slate-500 mr-3 h-8 w-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"&ndash;&gt;-->
+<!--                &lt;!&ndash;               xmlns="http://www.w3.org/2000/svg">&ndash;&gt;-->
+<!--                &lt;!&ndash;            <path&ndash;&gt;-->
+<!--                &lt;!&ndash;              d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"&ndash;&gt;-->
+<!--                &lt;!&ndash;              stroke-linecap="round" stroke-linejoin="round"&ndash;&gt;-->
+<!--                &lt;!&ndash;              stroke-width="2"/>&ndash;&gt;-->
+<!--                &lt;!&ndash;          </svg>&ndash;&gt;-->
+<!--                <div class="text-slate-800 text-2xl">-->
+<!--                    <strong>{ nameRubric }:</strong>-->
+<!--                </div>-->
+<!--            </div>-->
 
-            <hr>
-            <div class="mt-5 flex-grow flex flex-col">
-                <nav aria-label="Sidebar" class="flex-1 px-2 space-y-1">
-                    <!-- Current: "bg-slate-100 text-slate-900", Default: "text-slate-600 hover:bg-slate-50 hover:text-slate-900" -->
+<!--            <hr>-->
+<!--            <div class="mt-5 flex-grow flex flex-col">-->
+<!--                <nav aria-label="Sidebar" class="flex-1 px-2 space-y-1">-->
+<!--                    &lt;!&ndash; Current: "bg-slate-100 text-slate-900", Default: "text-slate-600 hover:bg-slate-50 hover:text-slate-900" &ndash;&gt;-->
 
-                    {#each category as {id, slug, name, product}}
-                    <a sveltekit:prefetch sveltekit:noscroll href='/products/{slug}'
-                              class="text-slate-800 hover:bg-slate-50 hover:text-slate-900 hover:shadow-md hover:shadow-slate-100/50 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
-                        <!-- Heroicon name: outline/folder -->
-                        <!--                <svg aria-hidden="true" class="text-slate-400 group-hover:text-slate-500 mr-3 h-6 w-6"-->
-                        <!--                     fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">-->
-                        <!--                  <path d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"-->
-                        <!--                        stroke-linecap="round"-->
-                        <!--                        stroke-linejoin="round"-->
-                        <!--                        stroke-width="2"/>-->
-                        <!--                </svg>-->
+<!--                    {#each category as {id, slug, name, product}}-->
+<!--                    <a sveltekit:prefetch sveltekit:noscroll href='/products/{slug}'-->
+<!--                              class="text-slate-800 hover:bg-slate-50 hover:text-slate-900 hover:shadow-md hover:shadow-slate-100/50 group flex items-center px-2 py-2 text-sm font-medium rounded-md">-->
+<!--                        &lt;!&ndash; Heroicon name: outline/folder &ndash;&gt;-->
+<!--                        &lt;!&ndash;                <svg aria-hidden="true" class="text-slate-400 group-hover:text-slate-500 mr-3 h-6 w-6"&ndash;&gt;-->
+<!--                        &lt;!&ndash;                     fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">&ndash;&gt;-->
+<!--                        &lt;!&ndash;                  <path d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"&ndash;&gt;-->
+<!--                        &lt;!&ndash;                        stroke-linecap="round"&ndash;&gt;-->
+<!--                        &lt;!&ndash;                        stroke-linejoin="round"&ndash;&gt;-->
+<!--                        &lt;!&ndash;                        stroke-width="2"/>&ndash;&gt;-->
+<!--                        &lt;!&ndash;                </svg>&ndash;&gt;-->
 
-                        &rarr; { name }
+<!--                        &rarr; { name }-->
 
-                        <span class="bg-red-700 text-slate-50 ml-auto inline-block py-0.5 px-2 text-xs font-medium rounded-full">
-                          { product.length }
-                        </span>
-                    </a>
-                    {/each}
-                </nav>
-            </div>
-        </div>
+<!--                        <span class="bg-red-700 text-slate-50 ml-auto inline-block py-0.5 px-2 text-xs font-medium rounded-full">-->
+<!--                          { product.length }-->
+<!--                        </span>-->
+<!--                    </a>-->
+<!--                    {/each}-->
+<!--                </nav>-->
+<!--            </div>-->
+<!--        </div>-->
+
+
+
+
+
+
+
+
+
+
+
+
 
         <!-- This example requires Tailwind CSS v2.0+ -->
         <div class="relative pt-4 pb-4 px-2 sm:px-6 lg:pt-4 lg:pb-8 lg:px-8">
 
+            <div class="">
+<!--                <div class="pt-12 sm:pt-16 lg:pt-20">-->
+<!--                    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">-->
+<!--                        <div class="text-center">-->
+<!--                            <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl lg:text-5xl">{ nameRubric }</h2>-->
+<!--                            {#if text}-->
+<!--                            <p class="mt-4 text-xl text-gray-600">{text.descriptionText}</p>-->
+<!--                                {:else }-->
+<!--                                <p class="mt-4 text-xl text-gray-600">Скидка пять проц</p>-->
+
+<!--                            {/if}-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+                <div class="mt-8 bg-white pb-16 sm:mt-12 sm:pb-20 lg:pb-28">
+                    <div class="relative">
+<!--                        <div class="absolute inset-0 h-1/2 bg-gray-100"></div>-->
+                        <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+
+                            {#each category as {id, name, slug, product, text}}
+                                <div class="max-w-lg mx-auto mb-8 rounded-lg shadow-lg overflow-hidden lg:max-w-none lg:flex ">
+                                <div class="flex-1 bg-white px-6 py-8 lg:p-12">
+                                    <h3 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">{name}</h3>
+                                    {#if (text)}
+                                    <p class="mt-6 text-base text-gray-500">{text.descriptionText}</p>
+                                        {:else }
+                                        <p class="mt-6 text-base text-gray-500">Цена снижена</p>
+
+                                        {/if}
+                                    <div class="mt-8">
+                                        <div class="flex items-center">
+                                            <h4 class="flex-shrink-0 pr-4 bg-white text-sm tracking-wider font-semibold uppercase text-indigo-700">Общая информация</h4>
+                                            <div class="flex-1 border-t-2 border-gray-200"></div>
+                                        </div>
+                                        <ul role="list" class="mt-8 space-y-5 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-5">
+                                            <li class="flex items-start lg:col-span-1">
+                                                <div class="flex-shrink-0">
+                                                    <!-- Heroicon name: solid/check-circle -->
+                                                    <svg class="h-5 w-5 text-red-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </div>
+                                                <p class="ml-3 text-sm text-gray-700">Оптовикам скидки</p>
+                                            </li>
+
+                                            <li class="flex items-start lg:col-span-1">
+                                                <div class="flex-shrink-0">
+                                                    <!-- Heroicon name: solid/check-circle -->
+                                                    <svg class="h-5 w-5 text-red-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </div>
+                                                <p class="ml-3 text-sm text-gray-700">Наличие на складе</p>
+                                            </li>
+
+                                            <li class="flex items-start lg:col-span-1">
+                                                <div class="flex-shrink-0">
+                                                    <!-- Heroicon name: solid/check-circle -->
+                                                    <svg class="h-5 w-5 text-red-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </div>
+                                                <p class="ml-3 text-sm text-gray-700">Доставка</p>
+                                            </li>
+
+                                            <li class="flex items-start lg:col-span-1">
+                                                <div class="flex-shrink-0">
+                                                    <!-- Heroicon name: solid/check-circle -->
+                                                    <svg class="h-5 w-5 text-red-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </div>
+                                                <p class="ml-3 text-sm text-gray-700">Безнал</p>
+                                            </li>
+
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="py-8 px-6 text-center bg-gray-50 lg:flex-shrink-0 lg:flex lg:flex-col lg:justify-center lg:p-12">
+                                    <p class="text-lg leading-6 font-medium text-gray-900">В этой товарной категории</p>
+                                    <div class="mt-4 flex items-center justify-center text-5xl font-extrabold text-gray-900">
+                                        <span> {product.length} </span>
+                                        <span class="ml-3 text-xl font-medium text-gray-500"> поз. </span>
+                                    </div>
+                                    <p class="mt-4 text-sm">
+                                        <a sveltekit:prefetch sveltekit:noscroll href='/products/{slug}' class="font-medium text-gray-500 underline"> Выберите нужную позицию </a>
+                                    </p>
+                                    <div class="mt-6">
+                                        <div class="rounded-md shadow">
+                                            <a sveltekit:prefetch sveltekit:noscroll href='/products/{slug}' class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-900"> Выбрать </a>
+                                        </div>
+                                    </div>
+                                    <div class="mt-4 text-sm">
+                                        <a href="/shop/cart" class="font-medium text-gray-900">
+                                            Перейти в корзину
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            {/each}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
             <!-- This example requires Tailwind CSS v2.0+ -->
             <div class="py-1 relative ">
 <!--            <div class="py-1 relative shadow-lg shadow-slate-100/50 rounded-lg border-2 border-slate-100 ring-offset-1 ring-1 ring-slate-50">-->
                 <!--          <div class="py-1 relative bg-gradient-to-b from-slate-50 via-slate-800 to-slate-50 shadow-lg rounded-lg">-->
+
+
 
 
 
@@ -153,8 +274,23 @@
                 <div class="mt-8 max-w-lg mx-auto lg:max-w-none">
 <!--                <div class="mt-8 max-w-lg mx-auto grid gap-5 lg:grid-cols-2 lg:max-w-none">-->
 
-                    {#each category as {id, name, slug, text}}
-                    <div class=" flex flex-col border-b-2">
+                    {#each category as {id, name, slug, product, text}}
+
+
+
+
+
+                        <!-- This example requires Tailwind CSS v2.0+ -->
+
+
+
+
+
+
+
+
+
+                        <div class=" flex flex-col border-b-2">
 <!--                    <div class=" flex flex-col rounded-lg shadow-lg shadow-slate-100/50 overflow-hidden border-2 border-slate-50 ring-offset-1 ring-1 ring-slate-50">-->
                         <!--            <div class="flex-shrink-0">-->
                         <!--              <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixqx=IrF17Golbw&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">-->
