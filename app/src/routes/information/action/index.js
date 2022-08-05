624150cb1c2c98ff280533ac2872af6891c40351
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-05 14:22:21 +0300

    git init

--- app/src/routes/information/action/index.js
+++ app/src/routes/information/action/index.js
@@ -23,24 +23,36 @@ const actions = [
     //     description: 'Акция действует 23 и 24 июля',
     //     link: '/products/mineralno-bazaltovye-utepliteli'
     // },
-    {
-        action: 'Пенопласт 25 плотности 100мм',
-        price: '215 рублей',
-        description: 'Предложение ограничено',
-        link: '/products/penoplast'
-    },
+    // {
+    //     action: 'Пенопласт 25 плотности 100мм',
+    //     price: '215 рублей',
+    //     description: 'Предложение ограничено',
+    //     link: '/products/penoplast'
+    // },
     {
         action: 'Плита ОСБ 9мм',
-        price: '540 рублей',
+        price: '530 рублей',
         description: 'Предложение ограничено',
-        link: '/products/penoplast'
+        link: '/product/plita-osb-3-250012509mm-kronospan-gost-vlagostoikaya'
     },
     {
-        action: 'Доска',
-        price: '16350 рублей за куб',
-        description: 'Только наличный платёж',
-        link: '/products/doska-obreznaya-obrabotannaya'
+        action: 'Софит-izol в (пароизоляция) 1,6м (35 м2)',
+        price: '635 рублей',
+        description: 'Предложение ограничено',
+        link: '/product/sofit-izol-v-paroizolyaciya-16m-35-m2'
     },
+    {
+        action: 'Софит-izol d (гидро-пароизоляция) 1,5м (70 м2)',
+        price: '1400 рублей',
+        description: 'Предложение ограничено',
+        link: '/product/sofit-izol-d-gidro-paroizolyaciya-15m-70-m2'
+    },
+    // {
+    //     action: 'Доска',
+    //     price: '16350 рублей за куб',
+    //     description: 'Только наличный платёж',
+    //     link: '/products/doska-obreznaya-obrabotannaya'
+    // },
 ]
 
 export const get = () => {
