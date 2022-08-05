6083f0aba2642bded1618ea6905daea6387b076b
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-05 14:28:48 +0300

    git init

--- app/src/routes/information/action/index.js
+++ app/src/routes/information/action/index.js
@@ -2,13 +2,13 @@ const actions = [
     {
         action: 'Утеплитель Техноблок 1200х600х100',
         price: '1290 рублей',
-        description: 'Акция действует одну неделю',
+        description: 'Только в эти выходные',
         link: '/product/uteplitel-texnoblok-standart-1200600100mm-0288-m3'
     },
     {
         action: 'Утеплитель Техноблок 1200х600х50',
         price: '1290 рублей',
-        description: 'Акция действует одну неделю',
+        description: 'Только в эти выходные',
         link: '/product/uteplitel-texnoblok-standart-120060050-mm-0288-m3'
     },
     // {
@@ -38,14 +38,26 @@ const actions = [
     {
         action: 'Софит-izol в (пароизоляция) 1,6м (35 м2)',
         price: '635 рублей',
-        description: 'Предложение ограничено',
+        description: 'Только в эти выходные',
         link: '/product/sofit-izol-v-paroizolyaciya-16m-35-m2'
     },
     {
         action: 'Софит-izol d (гидро-пароизоляция) 1,5м (70 м2)',
         price: '1400 рублей',
-        description: 'Предложение ограничено',
+        description: 'Только в эти выходные',
         link: '/product/sofit-izol-d-gidro-paroizolyaciya-15m-70-m2'
+    },
+    {
+        action: 'Штукатурная смесь "ек" tg 40 (30 кг)',
+        price: '420 рублей',
+        description: 'Предложение ограничено',
+        link: '/product/stukaturnaya-smes-ek-tg-40-30-kg'
+    },
+    {
+        action: 'Пена пистолетная "kudo home 65" всесезонная',
+        price: '410 рублей',
+        description: 'Предложение ограничено',
+        link: '/product/pena-pistoletnaya-kudo-home-65-vsesezonnaya'
     },
     // {
     //     action: 'Доска',
