fa620af0e15a8715b9b8641b3e59c25fc2f809fc
indexpro24@gmail.com <indexpro24@gmail.com>
2022-08-10 20:26:45 +0300

    git init

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -1,21 +1,21 @@
 const content_Swiper_v2 = [
     {
-        title_Marketing_v2: 'Цена - 1290 рублей',
+        title_Marketing_v2: 'Цена - 905 рублей',
         description_Marketing_v2:
-            'Утеплитель техноблок стандарт (1200*600*100мм), 0,288 м3. ТЕХНОБЛОК – это негорючие, гидрофобизированные тепло-, звукоизоляционные плиты из минеральной ваты на основе горных пород базальтовой группы. Наличие на складе уточняйте у наших менеджеров.',
+            'Утеплитель ISOBOX 50 мм, 0,288 м3. Изобокс – это негорючие, гидрофобизированные тепло-, звукоизоляционные плиты из минеральной ваты на основе горных пород базальтовой группы. Наличие на складе уточняйте у наших менеджеров.',
         img_Marketing_v2:
-            'https://lumen-image-bucket.s3.eu-central-1.amazonaws.com/images/a12adc654dac54907a50bc61812e8093.jpg',
-        alt_Marketing_v2: 'Утеплитель Техноблок',
-        link_Marketing_v2: '/shop/product/uteplitel-texnoblok-standart-1200600100mm-0288-m3'
+            'https://lumen-image-bucket.s3.eu-central-1.amazonaws.com/images/Elto2IOpobCUk46QmD8GOZmt6aGShwpbyBuphIWC.png',
+        alt_Marketing_v2: 'Утеплитель Изобокс',
+        link_Marketing_v2: '/product/uteplitel-izoboks-50-mm'
     },
     {
-        title_Marketing_v2: 'Цена - 530 рублей',
+        title_Marketing_v2: 'Цена - 520 рублей',
         description_Marketing_v2:
             'Плита осб-3 2500*1250*9мм kronospan гост (влагостойкая). ОСБ-3 (ОSB-3) 9x1250x2500 мм – представляет собой листовой облицовочный материал, предназначенный для черновых отделочных работ, а так же для оформления стен в некоторых дизайнах интерьеров.',
         img_Marketing_v2:
             'https://lumen-image-bucket.s3.eu-central-1.amazonaws.com/images/ilejI7dCV3zr72h4VudvlNrQLKBgHsrYwKd1HoAL.jpeg',
         alt_Marketing_v2: 'Плита ОСБ',
-        link_Marketing_v2: '/shop/product/plita-osb-3-250012509mm-kronospan-gost-vlagostoikaya'
+        link_Marketing_v2: '/product/plita-osb-3-250012509mm-kronospan-gost-vlagostoikaya'
     },
     {
         title_Marketing_v2: 'Цена - 420 рублей',
@@ -24,7 +24,7 @@ const content_Swiper_v2 = [
         img_Marketing_v2:
             'https://lumen-image-bucket.s3.eu-central-1.amazonaws.com/images/EK-TG40.png',
         alt_Marketing_v2: 'Штукатурка',
-        link_Marketing_v2: '/shop/product/stukaturnaya-smes-ek-tg-40-30-kg'
+        link_Marketing_v2: '/product/stukaturnaya-smes-ek-tg-40-30-kg'
     },
 ]
 
