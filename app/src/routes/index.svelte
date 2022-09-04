439bd03f00df928cace53f26dc3b35e991cc8296
indexpro24@gmail.com <indexpro24@gmail.com>
2022-09-04 22:47:26 +0300

    git init

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -119,7 +119,6 @@
 
 
 
-<!-- This example requires Tailwind CSS v2.0+ -->
 <div class="bg-white">
 	<main>
 
@@ -128,11 +127,11 @@
 
 
 
-		
+
 		<Swiper_v2 {content_Swiper_v2}/>
 
 		<div>
-			<!-- Hero card2 -->
+			<!-- Hero card3 -->
 <!--			<div class="relative">-->
 <!--				<div class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100"></div>-->
 <!--				<div class="max-w-full mx-auto sm:px-6 lg:px-8">-->
